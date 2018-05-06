624777fbf14701d484024cbf39b50d657ef3e356
Andy Wang <AndrewWang996@gmail.com>
2018-05-05 20:26:01 -0400

    updates to ImageWriter

--- src/main/java/ImageWriter.java
+++ src/main/java/ImageWriter.java
@@ -1,6 +1,14 @@
+import com.google.cloud.vision.v1.BoundingPoly;
+import com.google.cloud.vision.v1.Paragraph;
+import com.google.cloud.vision.v1.Vertex;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.util.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Font;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -17,15 +25,64 @@ public class ImageWriter {
         return img;
     }
 
-    private static BufferedImage overlayText(BufferedImage img, String text) {
-
+    private static BufferedImage overlayRect(
+            BufferedImage img,
+            Color rectColor,
+            int x, int y,
+            int w, int h
+    ) {
+        Graphics g = img.getGraphics();
+        g.setColor(rectColor);
+        g.fillRect(x, y, w, h);
+        return img;
     }
 
-    public static void main(String[] args) {
-//        BufferedImage bImg = loadImage("random_manga_images/dagashi_3.jpeg");
-//        Graphics G = Graphics.create(0, 0, 1000, 1000);
-//        G.drawImage(bImg, 0, 0, Color.GREEN, null);
+    private static BufferedImage overlayText(
+            BufferedImage img,
+            String text,
+            int textSize,
+            int x, int y
+            ) {
+        Graphics g = img.getGraphics();
+        g.setFont( new Font("Arial Black", Font.BOLD, textSize) );
+        g.drawString(text, x, y);
+        return img;
+    }
 
+    /**
+     *
+     * @param img
+     * @return
+     */
+    public static BufferedImage writeParagraphs(
+            BufferedImage img,
+            List<Paragraph> paragraphs
+    ) {
+        for (Paragraph p : paragraphs) {
+            BoundingPoly box = p.getBoundingBox();
+            Vertex topLeft = getTopLeft(box);
+            int x = topLeft.getX();
+            int y = topLeft.getY();
+            Vertex botRight = getBottomRight(box);
+            int w = Math.abs(x - botRight.getX());
+            int h = Math.abs(y - botRight.getY());
+
+
+
+            Color gray = Color.LIGHT_GRAY;
+            ColorSpace space = gray.getColorSpace();
+            float[] components = new float[3];
+            gray.getColorComponents(space, components);
+            Color rectColor = new Color(space, components, 0.5f);
+            overlayRect(img, rectColor, x, y, w, h);
+            // TODO: Replace color with method that determines color of background
+
+            // TODO: Write text
+        }
+        return img;
+    }
+
+    private static void displayImage(BufferedImage img) {
         JFrame window = new JFrame();
         window.setSize(1000, 1000);
         window.setTitle("Manga Translator");
@@ -33,17 +90,63 @@ public class ImageWriter {
         window.setVisible(true);
 
 
-        BufferedImage img = loadImage("random_manga_images/dagashi_3.jpeg");
-
         JPanel panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
+                System.out.println("painting the image");
                 g.drawImage(img, 0, 0, null);
+                System.out.println("painted the image");
             }
         };
 
         window.add(panel);
+        panel.setVisible(true);
+        window.setVisible(true);
+    }
 
+    private static Vertex getTopLeft(BoundingPoly box) {
+        int x,y;
+        x = y = (1 << 30);
+        for (Vertex v : box.getVerticesList()) {
+            if (v.getX() < x) { x = v.getX(); }
+            if (v.getY() < y) { y = v.getY(); }
+        }
+        return Vertex.newBuilder().setX(x).setY(y).build();
+    }
+
+    private static Vertex getBottomRight(BoundingPoly box) {
+        int x,y;
+        x = y = - (1 << 30);
+        for (Vertex v : box.getVerticesList()) {
+            if (v.getX() > x) { x = v.getX(); }
+            if (v.getY() > y) { y = v.getY(); }
+        }
+        return Vertex.newBuilder().setX(x).setY(y).build();
+    }
+
+    public static void main(String[] args) {
+
+        String filepath = "/Users/andywang/IdeaProjects/mangatranslator" +
+                "/random_manga_images/easy/hanebado.jpeg";
+        BufferedImage img = loadImage(filepath);
+        TextRecognizerGoogle txtRec = new TextRecognizerGoogle(Language.JPN);
+        java.util.List<Paragraph> paragraphs = new ArrayList<>();
+
+        try {
+            paragraphs = txtRec.detectDocumentText(filepath);
+        } catch(Exception e) {
+            System.err.printf("Exception %s caught!", e.toString());
+            System.exit(0);
+        }
+
+        Paragraph one = paragraphs.get(0);
+        System.out.println(one.getBoundingBox());
+        System.out.println(getTopLeft(one.getBoundingBox()));
+        System.out.println(getBottomRight(one.getBoundingBox()));
+        System.out.printf("Paragraphs length: %d\n", paragraphs.size());
+
+        writeParagraphs(img, paragraphs);
+        displayImage(img);
     }
 }
